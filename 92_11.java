55a9a7c07ba4d7b47da745476838ada374cf2719
satyachitti <42774533+satyachitti@users.noreply.github.com>
2018-09-10 15:44:21 +0100

    Create 92_11.java

---
+++ 92_11.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.*;
+import java.io.*;
+Class Ideone
+{
+	public static void main(Strings args[] args) throws java.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		int sum=0;
+		for(i}=0;i<n;i++)
+		sum+=a[i];
+		System.out,print(sum);
+	}
+	}
